9e68f2361a87bcdce178393198acdfd91a14e4d6
Arzhan Bakytbek <79097350+roses-are-dead666@users.noreply.github.com>
2021-08-19 16:16:13 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,39 @@
+function myFunctionText() {
+    document.getElementById('changeText').innerHTML = 
+    'Hello JavaScript!';
+}
+
+function myFunctionCat() {
+    document.getElementById('myImage').src = 
+    './assets/images/cat1.jpg';
+}
+
+function myFunctionDog() {
+    document.getElementById('myImage').src = 
+    './assets/images/dog1.jpg';
+}
+
+function myFunctionFz() {
+    document.getElementById('changeFz').style.fontSize = 
+    '30px';
+}
+
+function myFunctionNone() {
+    document.getElementById('changeStyle').style.display = 
+    'none';
+}
+
+function myFunctionBlock() {
+    document.getElementById('changeStyleBlock').style.display = 
+    'block';
+}
+
+function myFunctionSum() {
+    document.getElementById('statement').innerHTML = 
+    'Answer for this problem is = ' + z;
+}
+
+function myFunctionVar() {
+    document.getElementById('variable').innerHTML = 
+    'Answer for this problem is ' + total;
+}
