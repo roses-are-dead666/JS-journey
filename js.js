3da2959c6c4f5fa09edb6f46f355a3ec26ec16f9
Arzhan Bakytbek <79097350+roses-are-dead666@users.noreply.github.com>
2021-08-21 17:58:16 +0600

    Update js.js

--- js.js
+++ js.js
@@ -37,3 +37,79 @@ function myFunctionVar() {
     document.getElementById('variable').innerHTML = 
     'Answer for this problem is ' + total;
 }
+
+function myFunctionCon() {
+    document.getElementById('const').innerHTML = 
+    'List of cars: ' + cars;
+}
+
+function myFunctionCon2() {
+    document.getElementById('const2').innerHTML = 
+    'Car owner is: ' + car.owner;
+}
+
+function myFunctionOper() {
+    document.getElementById('oper').innerHTML = 
+    'Answer is ' + c;
+}
+
+function myFunctionOper2() {
+    document.getElementById('oper2').innerHTML = 
+    text1;
+}
+
+function myFunctionData() {
+    document.getElementById('data').innerHTML = 
+    (first == second) + "<br>" + (first == total);
+}
+
+var sum = myFunctionFun(4, 3);
+document.getElementById("fun").innerHTML = sum;
+
+function myFunctionFun(a, b) {
+    return a + b;
+}
+
+function toCelsius(f) {
+    return (5/9) * (f-32);
+}
+
+document.getElementById('fToC').innerHTML = 
+"The temperature is " + toCelsius(50) + " celsius";
+
+
+const motor = {type: 'Honda', model: 'Civic', color: 'blue'};
+document.getElementById("obj").innerHTML =
+"The car type is " + motor.type + " color is " + motor.color;
+
+const person = {
+    name: 'Olzhas',
+    surname: 'Mukanov',
+    age: 20,
+    status: 'retarded'
+};
+
+document.getElementById("olzhas").innerHTML = 
+person.name + " " + person.surname + " is " + person.age +
+" years old and his status is " + person.status;
+
+function displayDate() {
+    document.getElementById("elem").innerHTML = 
+    Date();
+}
+
+let text = "ikillniggasnocapwithgunheadshot"
+document.getElementById("str").innerHTML = 
+text.length;
+
+let string = "apple, banana, kiwi";
+document.getElementById("meth").innerHTML = 
+string.slice(7, 13);
+
+function changeWord(){
+    let strChange = document.getElementById("goF").innerHTML;
+    document.getElementById("goF").innerHTML = 
+    strChange.replace("yourself", "me");
+
+
+}
